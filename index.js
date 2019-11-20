6cf195ed7d5c43ba21625067d7bb70ba8304c044
mistermask <biggieboijosh@gmail.com>
2019-11-19 19:36:55 -0600

    got mongodb up and running.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var express = require('express')
+var mongodb = require('mongodb')
 
-var app = express()
+var url = 'mongodb+srv://Bello:Josh%40Master1@cluster0-fdog1.mongodb.net/test?retryWrites=true&w=majority'
+
+var mongoClient = mongodb.connect(url, (err, db) => {
+    console.log('data')
+    var dbo = db.db('data')
+    dbo.collection("goals").insertOne(testobj, (obj, res) => {
+        console.log('added')
+        db.close()
+    })
+})
+
+/*var app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {test: 'i swear this works'})
 })
 
-app.listen(3000, () => {console.log('working.')})
+app.listen(3000, () => {console.log('working.')})*/
